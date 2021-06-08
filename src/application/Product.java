f5b2e4a28c7def2373b21a06efdbdf1929c73b64
matisantillan11 <matias1.santillan@gmail.com>
2021-06-08 13:41:01 -0300

    adding dairy class with atr, constructors and getters. Modifying Product class

--- src/application/Product.java
+++ src/application/Product.java
@@ -1,10 +1,39 @@
 package application;
 
-public abstract class Product {
+public class Product {
     private int id;
     private String name;
     private String description;
     private float price;
     private int stock;
 
+    public Product(){};
+
+    public Product(int id, String name, String description, float price, int stock) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
 }
