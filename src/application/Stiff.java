f033eb7ea4e62dcbf75dcecb5dc5675aaa47a30a
nicolina182 <nicole.lizarraga@hotmail.com>
2021-06-08 15:01:12 -0300

    adding class Stiff with atr, getters and constructors

---
+++ src/application/Stiff.java
@@ -0,0 +1,24 @@
+package application;
+
+public class Stiff extends Product {
+    private String date_expiry;
+    private int fat_percentage;
+
+    public Stiff() {
+
+    }
+
+    public Stiff(int id, String name, String description, float price, int stock, String date_expiry, int fat_percentage) {
+        super(id, name, description, price, stock);
+        this.date_expiry = date_expiry;
+        this.fat_percentage = fat_percentage;
+    }
+
+    public String getDate_expiry() {
+        return date_expiry;
+    }
+
+    public int getFat_percentage() {
+        return fat_percentage;
+    }
+}
