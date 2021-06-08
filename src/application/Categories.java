063d41ccdda1da4dd826c6d53714d3d96e0c5582
nicolina182 <nicole.lizarraga@hotmail.com>
2021-06-08 13:00:14 -0300

    adding atr, constructor and getter in categories class

--- src/application/Categories.java
+++ src/application/Categories.java
@@ -1,5 +1,22 @@
 package application;
 
 public class Categories {
+     private int id;
+     private String name;
 
+    public Categories(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Categories() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
